919729ad7b14e6ed46903ff8964ee3cabe2ed30d
Giovanni De Toni <giovanni.det@gmail.com>
2015-08-30 13:11:00 +0200

    Added comments inside main.cc.

--- main.cc
+++ main.cc
@@ -5,26 +5,32 @@ using namespace std;
 #include "vigenere.h" 
 
 int main(int argc, char * argv[]) {
-  char fileName[] = "tabulaRecta";
+  char fileName[] = "tabulaRecta", c;
+  
+  /* Generate the matrix used by algorithm */
   generateTabula(fileName);
+  
+  /* Check if there are enough arguments */
   if (argc == 4) {
-    char c;
     cout << "Decrypt or Encrypt (d/e): ";
     cin >> c;
     switch(c) {
       case 'e':
+        /* Encrypt text */
         cryptText(argv[1], argv[2], argv[3]);
       break;
       case 'd':
+        /* Decrypt text */
         decryptText(argv[1], argv[2], argv[3]);
       break;
       default:
+        /* Exit if are supplied wrong letter */
         cout << "I didn't understand. Exiting";
         exit(1);
       break;  
     }
   } else {
-    cout << "Usage: input.txt output.txt encryption_key" << endl;
+    cout << "Usage: <input> <output> <encryption_key>" << endl;
     cout << "Exiting" << endl;
   }
   return 0; 
